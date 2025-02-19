7152db46433a7fbfbb5db6d306324f2e65ca097f
letsdothat <awer5424@naver.com>
2025-02-20 02:58:49 +0900

    Test: ProductRepositoryTest 테스트 추가
    Fixes : #1

--- src/test/java/com/e_co/e_commerce/product/ProductRepositoryTest.java
+++ src/test/java/com/e_co/e_commerce/product/ProductRepositoryTest.java
@@ -14,7 +14,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
 
@@ -51,41 +50,37 @@ public class ProductRepositoryTest {
         }
     }
 
-
-    // 페이지네이션, 조건, 정렬이 적용된 쿼리 테스트
     @Test
-    public void findAllByNameOrDescription() {
+    public void findAll() {
+        assertEquals(0, productRepository.count());
+        productRepository.saveAll(products);
+
+        List<Product> foundProducts = productRepository.findAll();
+        assertEquals(products.size(), foundProducts.size());
+
+        for (int i = 0; i < products.size(); i++) {
+            assertEquals(products.get(i), foundProducts.get(i));
+        }
+    }
+
+    // 페이지네이션 테스트
+    @Test
+    public void findAllWithPagination() {
         String filterName = "";
         String filterDescription = "";
-        boolean filterOnlyExist = false;
-
-        // 테스트 데이터를 자바 코드를 통해 쿼리에 맞춰 조건과 정렬 적용
-        List<Product> findProducts = products.stream().filter(product -> {
-            return (product.getName().contains(filterName) || product.getDescription().contains(filterDescription)) && product.getStock() >= (filterOnlyExist ? 1 : 0);
-        }).sorted(Comparator.comparingLong((Product p) -> p.getPrice()).reversed().thenComparing(p -> p.getId())).toList();
 
         int pageSize = 10;
-        int pageTotal = (int) (Math.ceil((double) findProducts.size() / pageSize));
+        int pageTotal = (int) (Math.ceil((double) products.size() / pageSize));
 
-        for (int i = 0; i < pageTotal; i++) {
-            Pageable pageable = PageRequest.of(i, pageSize, Sort.by("price").descending());
-            Page<Product> pages = productRepository.findAllByNameOrDescriptionOrStockGreaterThanEqual(filterName, filterDescription, filterOnlyExist ? 1 : 0, pageable);
+        productRepository.saveAll(products);
 
-            assertEquals(pageTotal, pages.getTotalPages());
-            assertEquals(pageSize, pages.getSize());
-            assertEquals(findProducts.size(), pages.getTotalElements());
+        for (int pageNumber = 0; pageNumber < pageTotal; pageNumber++) {
+            Pageable pageable = PageRequest.of(pageNumber, pageSize, Sort.by("price").descending());
 
-            // 현재 쿼리가 ID 순서를 보장하지 못해 쿼리로 나온 페이지와 자바 코드로 나온 페이지 요소가 달라
-            // 요소별 테스트가 힘듬
-            // 이미 검증된 Spring Data JPA Repository 테스트에 가까워서 급하진 않기때문에
-            // 우선 요소 테스트 코드 작성은 패스
-
-            //List<Product> pagedProducts = findProducts.subList(i * pageSize, (i < pageTotal - 1) ? (i + 1) * pageSize : findProducts.size());
-            //// DB Query가 Id 순서를 보장하지 않아 자바 코드로 임시로 정렬
-            //List<Product> realPagedProducts = pages.getContent().stream().sorted(Comparator.comparingLong((Product p) -> p.getPrice()).reversed().thenComparing(p -> p.getId())).toList();
-            //for (int j = 0; j < pagedProducts.size(); j++) {
-            //    assertEquals(pagedProducts.get(j), realPagedProducts.get(j));
-            //}
+            Page<Product> page = productRepository.findAllByNameOrDescriptionOrStockGreaterThanEqual(filterName, filterDescription, 0, pageable);
+            assertEquals(pageSize, page.getSize());
+            assertEquals(pageNumber, page.getNumber());
+            assertEquals(pageTotal, page.getTotalPages());
         }
     }
 }
